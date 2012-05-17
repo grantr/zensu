6d93fc2befe4368362d97860d9e3e9cf5e56bd28
Grant Rodgers <grantr@gmail.com>
2012-05-17 15:49:56 -0700

    notes on default settings

--- lib/zensu/settings.rb
+++ lib/zensu/settings.rb
@@ -7,6 +7,10 @@ module Zensu
       new MultiJson.load(File.read(filename))
     end
 
+    # TODO if there is no config, use default values.
+    # it should work to install the gem and run the server and client without any other setup.
+    # if redis is not configured, use a fakeredis persister. (with an appropriate warning against using this in production)
+
     def valid?
       #TODO validate required sections
       true
