b58eedf562c2be644dd3a5e512c29dc2e453176f
Grant Rodgers <grantr@gmail.com>
2012-05-20 15:19:03 -0700

    encode encrypted data with normal base64
    No need to be urlsafe

--- lib/zensu/rpc/ssl.rb
+++ lib/zensu/rpc/ssl.rb
@@ -25,12 +25,12 @@ module Zensu
       def public_encrypt(cert, string)
         #TODO should request object take care of this? - assume we have a key object here
         cert = cert.is_a?(String) ? OpenSSL::X509::Certificate.new(cert) : cert
-        Base64.urlsafe_encode64(cert.public_key.public_encrypt(string))
+        Base64.encode64(cert.public_key.public_encrypt(string))
       end
 
       def private_decrypt(key, string)
         key = key.is_a?(String) ? OpenSSL::PKey::RSA.new(key) : key
-        key.private_decrypt(Base64.urlsafe_decode64(string))
+        key.private_decrypt(Base64.decode64(string))
       end
 
       def generate_shared_key(length)
