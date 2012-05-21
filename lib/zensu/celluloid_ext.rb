62b56b68e9a58862cc9bc40364618ff299740311
Grant Rodgers <grantr@gmail.com>
2012-05-20 23:34:31 -0700

    add send_multiple to celluloid-zmq
    Allows to send multiple frames in a message, a common pattern for pubsub
    sockets.

--- lib/zensu/celluloid_ext.rb
+++ lib/zensu/celluloid_ext.rb
@@ -15,5 +15,23 @@ module Celluloid
         end
       end
     end
+
+    module WritableSocket
+      def send_multiple(messages)
+        messages.each_with_index do |message, i|
+          if i+1 == messages.size
+            flags = 0
+          else
+            flags = ::ZMQ::SNDMORE
+          end
+
+          unless ::ZMQ::Util.resultcode_ok? @socket.send_string(message, flags)
+            raise IOError, "error sending 0MQ message: #{::ZMQ::Util.error_string}"
+          end
+        end
+
+        messages
+      end
+    end
   end
 end
