184fb73fd5437f9bc2c4c3ffe67c44450b285c42
Grant Rodgers <grantr@gmail.com>
2012-05-20 17:02:24 -0700

    add handshake rpc specs

--- lib/zensu/rpc/handshake.rb
+++ lib/zensu/rpc/handshake.rb
@@ -10,18 +10,18 @@ module Zensu
         def handle(request)
           if valid_certificate?(request.cert)
             result = { 
-              'cert' => certificate.to_pem, 
+              'cert' => Zensu.settings.ssl.certificate, 
               'cipher' => Zensu.settings.ssl.cipher,
               'shared_key' => public_encrypt(request.cert, shared_key) }
             Response.new(result, nil, request.id)
           else
-            #TODO errors
-            Response.new("", "error", request.id)
+            Response.new(nil, "Invalid certificate", request.id)
           end
         end
 
         def shared_key
           @shared_key ||= generate_shared_key(cipher.key_len) #TODO get/set the shared key in redis
+          Zensu.settings.ssl.shared_key = @shared_key
         end
       end
 
@@ -32,18 +32,23 @@ module Zensu
         #TODO there should be an actor that manages authentication. all rpc actors are supervised by this class and restarted when it detects that handshake needs to happen again
 
         def request
-          Request.new("handshake", {name: Zensu.settings.client.name, cert: certificate.to_pem})
+          Request.new("handshake", {'name' => Zensu.settings.client.name, 'cert' => Zensu.settings.ssl.certificate})
         end
 
         def handle_response(response)
           #TODO retry if handshake failed
           if valid_certificate?(response.cert)
-            Zensu.settings.ssl.shared_key = private_decrypt(private_key, response.shared_key)
+            @shared_key = private_decrypt(private_key, response.shared_key)
+            Zensu.settings.ssl.shared_key = @shared_key
             Zensu.settings.ssl.cipher = response.cipher
           else
             Zensu.settings.ssl.shared_key = nil
           end
           #TODO state transition
+        end
+
+        def shared_key
+          @shared_key
         end
       end
     end
---
+++ spec/zensu/handshake_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe Zensu::RPC::Handshake::Keymaster do
+  before(:each) do
+    # for ssl certs
+    Zensu.settings = Zensu::Settings.load(config_file('config.json'))
+  end
+
+  it 'should return a successful response given a valid cert' do
+    request = Zensu::RPC::Request.new('handshake', {'name' => 'test', 'cert' => Zensu.settings.ssl.certificate})
+    response = subject.respond(request)
+    response.should be_success
+    response.result['cert'].should == Zensu.settings.ssl.certificate
+    response.result['cipher'].should == Zensu.settings.ssl.cipher
+    response.result['shared_key'].should_not be_nil
+  end
+
+  it 'should encrypt the shared key with the client cert' do
+    request = Zensu::RPC::Request.new('handshake', {'name' => 'test', 'cert' => Zensu.settings.ssl.certificate})
+    response = subject.respond(request)
+    subject.private_decrypt(subject.private_key, response.result['shared_key']).should == subject.shared_key
+  end
+
+  it 'should set the global shared key' do
+    request = Zensu::RPC::Request.new('handshake', {'name' => 'test', 'cert' => Zensu.settings.ssl.certificate})
+    response = subject.respond(request)
+    Zensu.settings.ssl.shared_key.should == subject.shared_key
+  end
+
+  it 'should return an error if cert was invalid' do
+    request = Zensu::RPC::Request.new('handshake', {'name' => 'test', 'cert' => File.read(config_file('invalid_cert.pem'))})
+    response = subject.respond(request)
+    response.should be_error
+  end
+
+end
+
+describe Zensu::RPC::Handshake::Keyslave do
+  it 'should generate handshake requests' do
+    request = subject.request
+    request.method.should == 'handshake'
+    request.name.should == Zensu.settings.client.name
+    request.cert.should == Zensu.settings.ssl.certificate
+  end
+
+  it 'should clear shared key if server cert is invalid' do
+    response = Zensu::RPC::Response.new('cert' => File.read(config_file('invalid_cert.pem')))
+    subject.handle_response(response)
+    Zensu.settings.ssl.shared_key.should be_nil
+  end
+
+  it 'should decrypt the returned shared key' do
+    response = Zensu::RPC::Response.new('cert' => Zensu.settings.ssl.certificate, 'shared_key' => subject.public_encrypt(Zensu.settings.ssl.certificate, '1'), 'cipher' => Zensu.settings.ssl.cipher)
+    subject.handle_response(response)
+    subject.shared_key.should == '1'
+  end
+
+  it 'should set the global shared key and cipher' do
+    response = Zensu::RPC::Response.new('cert' => Zensu.settings.ssl.certificate, 'shared_key' => subject.public_encrypt(Zensu.settings.ssl.certificate, '1'), 'cipher' => 'cipher')
+    subject.handle_response(response)
+    Zensu.settings.ssl.shared_key.should == '1'
+    Zensu.settings.ssl.cipher.should == 'cipher'
+  end
+end
