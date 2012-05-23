3166aaccf7bf965568991e6c8a4ed6ab2a35cfac
Grant Rodgers <grantr@gmail.com>
2012-05-22 19:14:32 -0700

    no keepalive timer reset
    Probably better to make keepalives more regular, at least at first while
    failure detection is still being worked out.

--- lib/zensu/client/keepalive_pusher.rb
+++ lib/zensu/client/keepalive_pusher.rb
@@ -2,18 +2,10 @@ module Zensu
   module Client
     class KeepalivePusher < Pusher
 
-      #TODO this should only send a keepalive if there were no pushes for 30 seconds.
-      # Every push should reset the keepalive timer.
-      # TODO every message received by the server should count as a keepalive 
-
       def initialize
         super
 
         @timer = every(30) { keepalive }
-      end
-
-      def timer
-        @timer
       end
 
       #TODO include timestamp and client configuration
