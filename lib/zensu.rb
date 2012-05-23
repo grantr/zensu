4130518e708ec8b2630d53f580b9d57aa598b79b
Grant Rodgers <grantr@gmail.com>
2012-05-23 12:20:39 -0700

    fix logger creation

--- lib/zensu.rb
+++ lib/zensu.rb
@@ -30,26 +30,29 @@ require 'zensu/client/keepalive_pusher'
 require 'zensu/client/subscriber'
 
 module Zensu
-  def self.settings=(settings)
-    @settings = settings
-  end
-
-  def self.settings
-    @settings ||= Zensu::Settings.new
-  end
-
-  def self.logger
-    unless @logger
-      log = Cabin::Channel.get
-      log.subscribe(STDOUT)
-      log.level = :debug #TODO configurable log level
-      self.logger = log
+  class << self
+    def settings=(settings)
+      @settings = settings
     end
-    @logger
-  end
 
-  # if logger = nil, make it a channel with no subscriptions
-  def self.logger=(log)
-    @logger = Celluloid.logger = (log ? log : Cabin::Channel.get) 
-  end
+    def settings
+      @settings ||= Zensu::Settings.new
+    end
+
+    def logger
+      unless @logger
+        log = Cabin::Channel.get
+        log.subscribe(STDOUT)
+        log.level = :debug #TODO configurable log level
+        self.logger = log
+      end
+      @logger
+    end
+
+    # if logger = nil, make it a channel with no subscriptions
+    def logger=(log)
+      @logger = Celluloid.logger = (log ? log : Cabin::Channel.get) 
+    end
+
+   end
 end
