e45799244a8cb91db898c39aa5ac873501e9506c
Grant Rodgers <grantr@gmail.com>
2013-01-09 11:28:42 -0800

    add server failure detection for clients

---
+++ lib/zensu/client/stethoscope.rb
@@ -0,0 +1,23 @@
+require 'zensu/client/failure_detector'
+
+module Zensu
+  module Client
+    class Stethoscope
+      include Celluloid
+      include Zensu::Broadcast
+
+      def initialize
+        @failure_detectors = {}
+        broadcast.subscribe(Actor.current, /^zensu.heartbeat/, :record_heartbeat)
+      end
+
+      #? Should this use subscriptions to route? We know the possible servers
+      # in advance. If server names were predictable, we could just start a
+      # failure detector for each one at boot.
+      def record_heartbeat(topic, server_id, heartbeat)
+        @failure_detectors[server_id] ||= FailureDetector.new_link(server_id)
+        @failure_detectors[server_id].add(Time.now.to_i)
+      end
+    end
+  end
+end
