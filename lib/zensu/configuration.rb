f8a6b435db3f399bd4d9509f49006bc0eb50e83c
Grant Rodgers <grantr@gmail.com>
2013-01-14 13:22:48 -0800

    restrict compile_methods! to valid method keys
    Actually a little more restrictive than that to avoid potential issues.

--- lib/zensu/configuration.rb
+++ lib/zensu/configuration.rb
@@ -80,9 +80,15 @@ module Zensu
     # compiles reader methods so we don't have to go through method_missing
     def self.compile_methods!(keys)
       keys.compact.reject { |m| method_defined?(m) }.each do |key|
-        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+        # only compile keys that are valid methods
+        # this rejects a few technically valid methods, but that's probably ok
+        # to allow ALL valid methods, use key.inspect !~ /[@$"]/
+        # Symbol will quote identifiers that are not valid (plus instance and global vars)
+        if key =~ /^[A-Za-z]\w*$/
+          class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def #{key}; get(:'#{key}'); end
-        RUBY
+          RUBY
+        end
       end
     end
 
