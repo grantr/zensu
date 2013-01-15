bed692bc948ed8f827378d5c5657685d53c78fbd
Grant Rodgers <grantr@gmail.com>
2013-01-14 23:23:05 -0800

    for now, publish the router_endpoint as heartbeat
    Ultimately the heartbeat will be a big bundle of stuff, but for now
    hardcode it.

--- lib/zensu/heart.rb
+++ lib/zensu/heart.rb
@@ -9,9 +9,11 @@ module Zensu
       every(HEARTBEAT) { beat }
     end
 
+    # heartbeat should include a list of referenceable actors and other things
+    # if heartbeat is encrypted, stethoscope should still work but it should ignore any data
     def beat
       Logger.trace "beat heart #{Zensu.node.id} #{Time.now.to_i}"
-      remote_publish("zensu.heartbeat", Zensu.node.id, Time.now.to_i.to_s)
+      remote_publish("zensu.heartbeat", Zensu.node.id, Zensu.config.router_endpoint, Time.now.to_i.to_s)
     end
   end
 end
