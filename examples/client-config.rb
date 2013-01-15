ed2b45dcba68b45ae7e01c48771caad0379f1d86
Grant Rodgers <grantr@gmail.com>
2013-01-14 23:33:21 -0800

    add examples of client and server configs

---
+++ examples/client-config.rb
@@ -0,0 +1,5 @@
+Zensu.node.signing_key = "8b9f1121df494bc03c6372210cbe17aa17d9afc90c975893a59b21dadfd60133"
+
+Zensu.configure do |c|
+  c.broadcast_servers = ["tcp://127.0.0.1:58000"]
+end
---
+++ examples/server-config.rb
@@ -0,0 +1,7 @@
+Zensu.node.signing_key = "73c24ed4d6bab71537dc0a85ee339be1d861e7224e2b112895c17dc2905fb3ac"
+
+Zensu.configure do |c|
+  c.broadcast_endpoint = "tcp://127.0.0.1:58000"
+  c.router_endpoint = "tcp://127.0.0.1:48000"
+end
+
