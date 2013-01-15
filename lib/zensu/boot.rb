4fc841523d633108dd72a7284cad2ff650f54edf
Grant Rodgers <grantr@gmail.com>
2013-01-14 19:09:24 -0800

    no incident reporter for early development
    In the experimentation phase, only an event reporter is needed

--- lib/zensu/boot.rb
+++ lib/zensu/boot.rb
@@ -1,7 +1,7 @@
 Celluloid::ZMQ.init
 
 Celluloid.logger = Celluloid::IncidentLogger.new
-Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
+# Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
 Celluloid::EventReporter.supervise_as :default_event_reporter, STDOUT
 
 Zensu::RemoteNotifier.supervise_as :remote_notifier
